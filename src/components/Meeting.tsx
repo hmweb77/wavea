ed26ce86c9245d481721e5bd6d28ce11e0a6c66a
hmweb77 <oussamahamdani1@gmail.com>
2024-01-07 13:59:25 +0000

    setting the project and navBar

---
+++ src/components/Meeting.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import pic1 from "../assets/mobile-picture.svg"
+import pic2 from "../assets/ux-picture.svg"
+import pic3 from "../assets/dev-picture.svg"
+import pic4 from "../assets/marketing-picture.svg"
+import pic5 from "../assets/calendar.svg"
+import Image from 'next/image'
+export default function Meeting() {
+  return (
+    <div>
+        <h1>meeting</h1>
+      
+    </div>
+  )
+}
